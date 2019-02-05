a7644ac789c9110357819c4eb36218da3a9325ae
Andrés Osante <aosantea@ucenfote.ac.cr>
2019-02-04 22:37:24 -0600

    testimonials section done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //parallax effect
 const parallaxBg = document.querySelector('.parallax-bg');
+const parallaxBg2 = document.querySelector('.parallax-bg2');
 window.addEventListener('scroll', parallax);
 let didScroll = false;
 
@@ -8,10 +9,23 @@ function parallax() {
 }
 
 setInterval(() => {
+  let windowWidth = window.innerWidth;
   if (didScroll) {
     let scroll = window.scrollY;
     parallaxBg.style.backgroundPosition = 'center ' + scroll * 0.45 + 'px';
+    switch (true) {
+      case windowWidth >= 1180:
+        parallaxBg2.style.backgroundPosition =
+          'center ' + (scroll - 3500) * 0.25 + 'px';
+        break;
+      case windowWidth < 1180 && windowWidth > 1050:
+        parallaxBg2.style.backgroundPosition =
+          'center ' + (scroll - 900) * 0.25 + 'px';
+        break;
+      case windowWidth < 940:
+        parallaxBg2.style.backgroundPosition =
+          'center ' + (scroll - 1380) * 0.25 + 'px';
+    }
     didScroll = false;
-    console.log('you scrolled');
   }
 }, 9);
