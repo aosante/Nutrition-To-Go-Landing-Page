05f5361f29843b19c4b99a80fc1b78bbdab9ec8b
Andrés Osante <aosantea@ucenfote.ac.cr>
2019-02-07 22:12:36 -0600

    finished media queries

--- js/app.js
+++ js/app.js
@@ -1,31 +1,23 @@
 //parallax effect
 const parallaxBg = document.querySelector('.parallax-bg');
 const parallaxBg2 = document.querySelector('.parallax-bg2');
-window.addEventListener('scroll', parallax);
-let didScroll = false;
+window.addEventListener('scroll', _.debounce(parallax));
 
 function parallax() {
-  didScroll = true;
-}
-
-setInterval(() => {
   let windowWidth = window.innerWidth;
-  if (didScroll) {
-    let scroll = window.scrollY;
-    parallaxBg.style.backgroundPosition = 'center ' + scroll * 0.45 + 'px';
-    switch (true) {
-      case windowWidth >= 1180:
-        parallaxBg2.style.backgroundPosition =
-          'center ' + (scroll - 3500) * 0.25 + 'px';
-        break;
-      case windowWidth < 1180 && windowWidth > 1050:
-        parallaxBg2.style.backgroundPosition =
-          'center ' + (scroll - 900) * 0.25 + 'px';
-        break;
-      case windowWidth < 940:
-        parallaxBg2.style.backgroundPosition =
-          'center ' + (scroll - 1380) * 0.25 + 'px';
-    }
-    didScroll = false;
+  let scroll = window.scrollY;
+  parallaxBg.style.backgroundPosition = 'center ' + scroll * 0.45 + 'px';
+  switch (true) {
+    case windowWidth >= 1180:
+      parallaxBg2.style.backgroundPosition =
+        'center ' + (scroll - 3500) * 0.25 + 'px';
+      break;
+    case windowWidth < 1180 && windowWidth > 1050:
+      parallaxBg2.style.backgroundPosition =
+        'center ' + (scroll - 900) * 0.25 + 'px';
+      break;
+    case windowWidth < 940:
+      parallaxBg2.style.backgroundPosition =
+        'center ' + (scroll - 1380) * 0.25 + 'px';
   }
-}, 9);
+}
