daf960b2cb6bf2a56b3516210ded68cf6580a191
Andrés Osante <aosantea@ucenfote.ac.cr>
2019-02-16 22:22:42 -0600

    added favicon

--- server.js
+++ server.js
@@ -18,7 +18,46 @@ app.use(bodyParser.json());
 
 //Nodemailer code
 //---------------------------------------------
-//JejeXD
+app.post('/send-email', (req, res) => {
+  //create an output variable for the html that is going to be sent
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+      <li>Nombre: ${req.body.nombre}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Referencia: ${req.body.referencia}</li>
+    </ul>
+    <h3>Mensaje</h3>
+    <p>${req.body.mensaje}</p>
+  `;
+  const CONTACT_ADDRESS = 'contact@gmail.com';
+
+  //site owner addsd credentials here
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'contact@gmail.com',
+      pass: 'XXXXX'
+    }
+  });
+
+  let mailOptions = {
+    from: req.body.email, // sender address
+    to: [CONTACT_ADDRESS], // list of receivers
+    subject: 'Node contact request', // Subject line
+    text: req.body.mensaje, // plain text body
+    html: output // html body
+  };
+  //send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+    res.sendFile(path.join(__dirname + '/index.html'));
+  });
+});
 
 //---------------------------------------------
 
